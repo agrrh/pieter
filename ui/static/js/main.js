0aca59e0f54bddc5e419aca4ed35076dd0280144
Kirill K <kovalev.kirill.a@gmail.com>
2018-01-09 19:17:22 +0300

    Save progress with UI

--- ui/static/js/main.js
+++ ui/static/js/main.js
@@ -26,23 +26,37 @@ function hideLoading() {
   $("#repos-loading").hide();
 }
 
+function hideRepos() {
+  $("#repos").hide();
+}
+
 function displayRepo(repo) {
+  $("#repo").show();
+  $('#repo').removeClass('three column').addClass('one column');
   $.get(api_url + "repos/" + repo, function(data) {
     console.log(data);
-    $('#repo-template').tmpl(data).appendTo('#repos');
+    $("#repo-loading").hide();
+    $('#repo-template').tmpl(data).appendTo('#repo');
+  });
+}
+
+function displayReposRepo(repo) {
+  $.get(api_url + "repos/" + repo, function(data) {
+    $('#repos-repo-template').tmpl(data).appendTo('#repos');
     displayRepoScenario(data['name'], data['scenarios']);
   });
 }
 
 function displayRepoScenario(repo, scenario) {
-  console.log(repo, scenario);
-  $("<div class=\"ui grey label\"><i class=\"code icon\"></i> "+scenario+"</div>").appendTo('#repo-' + repo + '-scenarios');
+  scenario.forEach(function(item, i, arr) {
+    $("#repo-scenario-template").tmpl({'scenario': item}).appendTo('#repo-' + repo + '-scenarios');
+  });
 }
 
 function getReposList() {
   $.get(api_url + "repos", function(data) {
     data.forEach(function(item, i, arr) {
-      displayRepo(item);
+      displayReposRepo(item);
     });
   });
 }
